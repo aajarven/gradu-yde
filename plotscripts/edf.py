68e7e023763c9c392f9cb5c8dfacfd1d503e8a6e
aajarven <anni.jarvenpaa@gmail.com>
2017-06-01 04:11:34 +0300

    some edits, not good yet

--- plotscripts/edf.py
+++ plotscripts/edf.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import math
+import pylab
 import random
 
 random.seed(715517)
@@ -14,21 +15,35 @@ normalNumbersLow = np.random.normal(5.0, 2.0, N)
 normalNumbersWide = np.random.normal(10.0, 4.0, N)
 normalNumbersWide2 = np.random.normal(10.0, 4.0, N)
 
+biggest = max(max(max(np.amax(normalNumbers), np.amax(normalNumbersLow)),
+        np.amax(normalNumbersWide)), np.amax(normalNumbersWide2))
+
 percentage = np.arange(0, 100, 100.0/N)
 percentage = np.append(percentage, 100)
 
-f, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+f.subplots_adjust(hspace=0)
 
-ax2.hist(normalNumbers, histtype='step', stacked=True, fill=False)
-ax2.hist(normalNumbersLow, histtype='step', stacked=True, fill=False)
+bins = np.arange(-2, biggest+1, 1.5)
+ax2.hist(normalNumbers, bins, histtype='step', stacked=True, fill=False,
+        linewidth=2.0)
+ax2.hist(normalNumbersLow, bins, histtype='step', stacked=True, fill=False,
+        linewidth=2.0)
+ax2.hist(normalNumbersWide, bins, histtype='step', stacked=True, fill=False,
+        linewidth=2.0)
+ax2.hist(normalNumbersWide2, bins, histtype='step', stacked=True, fill=False,
+        linewidth=2.0)
+
+# hide topmost ticklabel on lower plot
+lastTickLabel = ax2.get_yticklabels()
+lastTickLabel = lastTickLabel[len(lastTickLabel)-1]
+plt.setp(lastTickLabel, visible=False)
 
 normalNumbers.sort()
 normalNumbersLow.sort()
 normalNumbersWide.sort()
 normalNumbersWide2.sort()
 
-biggest = max(max(max(np.amax(normalNumbers), np.amax(normalNumbersLow)),
-        np.amax(normalNumbersWide)), np.amax(normalNumbersWide2))
 normalNumbers = np.append(normalNumbers, biggest)
 normalNumbersLow = np.append(normalNumbersLow, biggest)
 normalNumbersWide = np.append(normalNumbersWide, biggest)
@@ -43,14 +58,16 @@ ax1.plot(normalNumbersWide, percentage, label='$\mu$=10.0, $\sigma$=4.0',
 ax1.plot(normalNumbersWide2, percentage, label='$\mu$=10.0, $\sigma$=4.0',
 		 linewidth=2.0)
 
-ax2.xlabel('values')
-ax1.ylabel('CDF')
+plt.xlabel('values')
+#ax1.ylabel('CDF')
 ax1.legend(loc=0)
-ax1.xlim([-2, math.ceil(biggest)])
-ax2.xlim([-2, math.ceil(biggest)])
+plt.xlim([-2, math.ceil(biggest)])
 
 rc('font', **{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
-plt.show()
-#plt.savefig('../kuvat/edf.png')
+F = pylab.gcf()
+F.set_size_inches(5.90666, 8)
+
+#plt.show()
+plt.savefig('../kuvat/edf.png')
