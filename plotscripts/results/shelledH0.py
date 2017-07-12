041d3437ebe8b9114b72aa709719e9a8e9a3de87
aajarven <anni.jarvenpaa@gmail.com>
2017-07-12 19:10:43 +0300

    zeros myös

--- plotscripts/results/shelledH0.py
+++ plotscripts/results/shelledH0.py
@@ -19,14 +19,16 @@ from matplotlib import rc
 
 if __name__ == "__main__":
 	inputfile = "../input/lgfound-fullpath.txt" 
-	saveloc = "../../kuvat/shelledH0.svg"
+	savelocH0 = "../../kuvat/shelledH0.svg"
+	savelocZero = "../../kuvat/zeros.svg"
 
 	lines =  sum(1 for line in open(inputfile))
-	limits = [[d, d+2.0] for d in np.arange(0.0, 8.0, 0.1)]
-	H0 = np.full((lines, len(limits)), np.nan)
-	H0min = np.full((1, len(limits)), np.nan)
-	H0max = np.full((1, len(limits)), np.nan)
-	zeros = np.full((lines, len(limits)), np.nan)
+	limitsH0 = [[d, d+2.0] for d in np.arange(0.0, 8.0, 0.1)]
+	limitsZeros = [[d, d+4] for d in np.arange(0.0, 6.0, 1.0)]
+	H0 = np.full((lines, len(limitsH0)), np.nan)
+	H0min = np.full((1, len(limitsH0)), np.nan)
+	H0max = np.full((1, len(limitsH0)), np.nan)
+	zeros = np.full((lines, len(limitsZeros)), np.nan)
 
 	f = open(inputfile, 'r')
 	sim = -1
@@ -94,8 +96,8 @@ if __name__ == "__main__":
 						   for j in range(len(vel))])
 
 
-		##### extracting interesting data starts #####
-		for limit, index in zip(limits, range(len(limits))):
+		##### extracting H0 #####
+		for limit, index in zip(limitsH0, range(len(limitsH0))):
 			# jump to next if the haloes don't go all the way to the outer limit
 			if max(distances) < limit[1]:
 				continue
@@ -105,17 +107,26 @@ if __name__ == "__main__":
 			k, b, r_value, p_value, std_err = linregress(distances[mask],
 													  radvel[mask])
 			H0[sim, index] = k 
+
+		for limit, index in zip(limitsZeros, range(len(limitsZeros))):
+			if max(distances) < limit[1]:
+				continue
+
+			mask = np.array([d > limit[0] and d < limit[1] for d in distances])
+			k, b, r, p, std_err = linregress(distances[mask], radvel[mask])
 			zeros[sim, index] = -b/k
+
+
 	
 	# standard errors
-	for i in range(len(limits)):
+	for i in range(len(limitsH0)):
 		data = H0[:, i]
 		data = data[~np.isnan(data)]
 		sem = scipy.stats.sem(data)
 		H0min[0, i] = np.nanmean(data) - sem
 		H0max[0, i] = np.nanmean(data) + sem
 
-	centers = [(l[0]+l[1])/2 for l in limits]
+	centers = [(l[0]+l[1])/2 for l in limitsH0]
 	
 	#### plotting #####
 	fig = plt.figure()
@@ -124,7 +135,7 @@ if __name__ == "__main__":
 	plt.plot(centers, H0min.flatten(), linewidth=2.0, color='0.75')
 	plt.plot(centers, np.nanmean(H0, axis=0), linewidth=2.0, color='k')
 	
-	plt.xlabel("Distance of bin centre from LG centre (Mpc)")
+	plt.xlabel("Distance of bin centre from Milky Way (Mpc)")
 	plt.ylabel("Mean H0 when fitted using haloes in bin")	
 	
 	rc('font', **{'family':'serif','serif':['Palatino']})
@@ -133,4 +144,28 @@ if __name__ == "__main__":
 
 	fig.set_size_inches(5.9, 5)
 	
-	plt.savefig(saveloc)
+	plt.savefig(savelocH0)
+
+
+	plt.cla()
+	plt.clf()
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	
+	mask = ~np.isnan(zeros)
+	filteredZeros = [d[m] for d,m in zip(zeros.T, mask.T)]
+	centers = [(l[0]+l[1])/2.0 for l in limitsZeros]
+	plt.boxplot(filteredZeros)
+	ax.set_xticklabels(centers)
+	plt.ylim([-2, 4])
+
+	plt.xlabel("Distance of bin centre from Milky Way (Mpc)")
+	plt.ylabel("Hubble flow zero point distance from MW (Mpc)")	
+	
+	rc('font', **{'family':'serif','serif':['Palatino']})
+	rc('text', usetex=True)
+	plt.tight_layout()
+
+	fig.set_size_inches(5.9, 5)
+
+	plt.savefig(savelocZero)
