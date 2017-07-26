59619520101d7f794b21ffd21ae9710e66b419c4
aajarven <anni.jarvenpaa@gmail.com>
2017-07-26 17:01:03 +0300

    ready?

--- plotscripts/results/clusteringFinds.py
+++ plotscripts/results/clusteringFinds.py
@@ -18,18 +18,18 @@ from sklearn.cluster import DBSCAN
 
 if __name__ == "__main__":
 
-	inputfile = "../input/hyvat-fullpath.txt"
+	inputfile = "../input/lgfound-fullpath.txt"
 	saveloc = "../../kuvat/clusteringParameters.svg"
 
 	simIndex = 0
 
-	samples = 50
+	samples = 32
 	minEPS = 0.5 
-	maxEPS = 13.0
+	maxEPS = 8.5
 	EPSvalues = np.arange(minEPS, maxEPS+(maxEPS-minEPS)/samples,
 					   (maxEPS-minEPS)/samples)
 	minMS = 1
-	maxMS = 21
+	maxMS = 35
 	MSvalues = np.arange(minMS, maxMS+1)
 	clusters = np.zeros((len(EPSvalues), len(MSvalues)))
 
@@ -102,19 +102,29 @@ if __name__ == "__main__":
 	np.savetxt("/home/aajarven/Z-drive/duuni/extragal/gradu-yde/plotscripts/tmp-out/meanClusters.txt",
 			meanClusters)
 
+	# replace zeroes with small value for log plotting
+	meanClusters[meanClusters == 0] = np.finfo(np.float).eps
+
 	matplotlib.rcParams['axes.unicode_minus'] = False
+	rc('font', **{'family':'serif','serif':['Palatino']})
+	rc('text', usetex=True)
 
 	fig = plt.gcf()
 	ax = plt.gca()
 
+	pcm = ax.pcolormesh(MSvalues, EPSvalues, meanClusters, cmap='magma',
+					 vmin=1, vmax=20, edgecolors='face')
+#					 norm=colors.LogNorm(vmin=1, vmax=20))
+	cb = fig.colorbar(pcm, ax=ax, extend='both', ticks=[1, 5, 10, 15, 20],
+				  label='Mean number of clusters found')
+	cb.ax.set_yticklabels([1, 5, 10, 15, 20])
 
-	pcm = ax.pcolormesh(MSvalues, EPSvalues, meanClusters,
-					 norm=colors.LogNorm(vmin=1, vmax=30), cmap='viridis')
-	cb = fig.colorbar(pcm, ax=ax, extend='both')
-	cb.ax.minorticks_on()
+	minorticks = pcm.norm(np.arange(1, 20, 1))
+	cb.ax.yaxis.set_ticks(minorticks, minor=True)
 
-	xticks = np.arange(np.ceil(min(MSvalues)), np.floor(max(MSvalues)), 2)
-	yticks = np.arange(np.ceil(min(EPSvalues)), np.floor(max(EPSvalues)), 1)
+
+	xticks = np.arange(2, max(MSvalues), 2)
+	yticks = np.arange(np.ceil(min(EPSvalues)), max(EPSvalues), 1)
 
 	ax.axis([min(MSvalues), max(MSvalues), min(EPSvalues), max(EPSvalues)])
 	ax.set_xticks(xticks + 0.5*(MSvalues[1]-MSvalues[0]), minor=False)
@@ -124,19 +134,19 @@ if __name__ == "__main__":
 	ax.set_xlim(min(MSvalues), max(MSvalues))
 	ax.set_ylim(min(EPSvalues), max(EPSvalues))
 
+#	ax.set_adjustable("box-forced")
 
-	ax.set_adjustable("box-forced")
+	ax.set_xlabel("Minsamples")
+	ax.set_ylabel(r"$\varepsilon$ (mean distances to neighbour)",
+			   multialignment='center')
 
 #	plt.imshow(meanClusters, interpolation='nearest', cmap='viridis')
 #	plt.colorbar()
 	
 
-	rc('font', **{'family':'serif','serif':['Palatino']})
-#	rc('text', usetex=True)
-
 	plt.tight_layout()
 
 
-	fig.set_size_inches(4.2, 4.0)
+	fig.set_size_inches(5.9, 4.0)
 
 	plt.savefig(saveloc)
