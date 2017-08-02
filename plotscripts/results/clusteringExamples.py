c3998f113e8330bd82bd85d87c69ad7c7eb12c93
aajarven <anni.jarvenpaa@gmail.com>
2017-08-02 16:51:37 +0300

    effect of variations added, actual plotting defined as a procedure

--- plotscripts/results/clusteringExamples.py
+++ plotscripts/results/clusteringExamples.py
@@ -8,9 +8,37 @@ import clustering
 import LGfinder
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from matplotlib import gridspec
 import numpy as np
 import physUtils
 from sklearn.cluster import DBSCAN
+
+# HUOM OBS NB: kaatuu jos colours on liian lyhyt
+def loopClusterPlotting(fitdata, axes, clusteringParameters, colours):
+	for ax, parameters in zip(axes, clusteringParameters):
+		eps = clustering.meanSeparationEps(fitdata, parameters[1])
+		db = DBSCAN(eps=eps, min_samples=parameters[0], metric='precomputed', ).fit(fitdata)
+		labels = db.labels_
+		uniqueLabels = set(labels)
+		clusters = len(set(labels)) - (1 if -1 in labels else 0)
+
+		for l in uniqueLabels:
+			if l == -1:
+				col = 'k'
+				size = 1
+			else:
+				col = colours[l]
+				size = 16
+			
+			mask = (labels == l)
+			members = directions[mask]
+			ax.scatter(members[:, 0], members[:, 1], facecolors=col,
+			  edgecolors='k', s=size)
+			ax.grid(b=True)
+			ax.set_xticklabels([])
+			ax.set_yticklabels([])
+			ax.set_title(r"$\varepsilon$=" + str(parameters[1]) + ", minsamples=" +
+				str(parameters[0]))
 
 if __name__ == "__main__":
 
@@ -46,37 +74,16 @@ if __name__ == "__main__":
 
 	fitdata = clustering.precomputeDistances(directions)
 
-	
+	# general examples
+
 	clusteringParameters = [(6, 4.5), (15, 4.5),
 						 (6, 2.0), (15, 2.0),
 						 (6, 1.0), (15, 1.0)]
 	f, axarr = plt.subplots(3, 2, subplot_kw=dict(projection='mollweide'))
 	colours = plt.cm.Set1(np.linspace(0, 1, 14))
 	
-	for ax, parameters in zip(axarr.flatten(), clusteringParameters):
-		eps = clustering.meanSeparationEps(fitdata, parameters[1])
-		db = DBSCAN(eps=eps, min_samples=parameters[0], metric='precomputed', ).fit(fitdata)
-		labels = db.labels_
-		uniqueLabels = set(labels)
-		clusters = len(set(labels)) - (1 if -1 in labels else 0)
-
-		for l in uniqueLabels:
-			if l == -1:
-				col = 'k'
-				size = 1
-			else:
-				col = colours[l] # oletetaan alle 12 klusteria, muulloin kaatuu
-				size = 16
-			
-			mask = (labels == l)
-			members = directions[mask]
-			ax.scatter(members[:, 0], members[:, 1], facecolors=col,
-			  edgecolors='k', s=size)
-			ax.grid(b=True)
-			ax.set_xticklabels([])
-			ax.set_yticklabels([])
-			ax.set_title(r"$\varepsilon$=" + str(parameters[1]) + ", minsamples=" +
-				str(parameters[0]))
+	loopClusterPlotting(fitdata, axarr.flatten(), clusteringParameters,
+					 colours)
 	
 	
 	rc('font', **{'family':'serif','serif':['Palatino']})
@@ -88,3 +95,28 @@ if __name__ == "__main__":
 	f.set_size_inches(5.9, 5.3)
 
 	plt.savefig(saveloc + "clusteringExamples.svg")
+
+
+	# examples of values near the chosen value
+	plt.cla()
+	plt.clf()
+
+	gs = gridspec.GridSpec(3, 2, height_ratios=[3.9, 2, 2])
+	bigax = plt.subplot(gs[0, :], projection="mollweide")
+	smallax1 = plt.subplot(gs[1, 0], projection="mollweide")
+	smallax2 = plt.subplot(gs[1, 1], projection="mollweide")
+	smallax3 = plt.subplot(gs[2, 0], projection="mollweide")
+	smallax4 = plt.subplot(gs[2, 1], projection="mollweide")
+
+	clusteringParameters = [(10, 1.8),
+						 (10, 1.6), (10, 2.0),
+						 (8, 1.8), (12, 1.8)]
+	loopClusterPlotting(fitdata, [bigax, smallax1, smallax2, smallax3,
+							   smallax4], clusteringParameters, colours)
+
+	rc('font', **{'family':'serif','serif':['Palatino']})
+	rc('text', usetex=True)
+	
+	f.set_size_inches(5.9, 7.0)
+	
+	plt.savefig(saveloc + "smallClusteringVariations.svg")
