5832c7b86fbe60d0acf4861dfd0961a2fb11db92
aajarven <anni.jarvenpaa@gmail.com>
2017-08-02 15:01:34 +0300

    toimiva

--- plotscripts/results/clusteringExamples.py
+++ plotscripts/results/clusteringExamples.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import LGfinder
-import math
-import matplotlib.pyplot as plt
-from matplotlib import gridspec
-import numpy as np
-from optparse import OptionParser
-import os
-import pylab
-import physUtils
-import random
-from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
 import sys
-from transitiondistance import findBestHubbleflow
+sys.path.insert(0, '/scratch/aajarven/plotscripts/')
+
+import clustering
+import LGfinder
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import numpy as np
+import physUtils
+from sklearn.cluster import DBSCAN
 
 if __name__ == "__main__":
 
@@ -48,8 +44,47 @@ if __name__ == "__main__":
 	directions = np.array([physUtils.sphericalCoordinates(pos - centre) for
 						   pos in cop])
 
-	fitdata = precomputeDistances(directions)
-   
-	db = DBSCAN(eps=eps, min_samples=ms, metric='precomputed', ).fit(fitdata)
-	labels = db.labels_
-	clusters = len(set(labels)) - (1 if -1 in labels else 0) 
+	fitdata = clustering.precomputeDistances(directions)
+
+	
+	clusteringParameters = [(6, 4.5), (15, 4.5),
+						 (6, 2.0), (15, 2.0),
+						 (6, 1.0), (15, 1.0)]
+	f, axarr = plt.subplots(3, 2, subplot_kw=dict(projection='mollweide'))
+	colours = plt.cm.Set1(np.linspace(0, 1, 14))
+	
+	for ax, parameters in zip(axarr.flatten(), clusteringParameters):
+		eps = clustering.meanSeparationEps(fitdata, parameters[1])
+		db = DBSCAN(eps=eps, min_samples=parameters[0], metric='precomputed', ).fit(fitdata)
+		labels = db.labels_
+		uniqueLabels = set(labels)
+		clusters = len(set(labels)) - (1 if -1 in labels else 0)
+
+		for l in uniqueLabels:
+			if l == -1:
+				col = 'k'
+				size = 1
+			else:
+				col = colours[l] # oletetaan alle 12 klusteria, muulloin kaatuu
+				size = 16
+			
+			mask = (labels == l)
+			members = directions[mask]
+			ax.scatter(members[:, 0], members[:, 1], facecolors=col,
+			  edgecolors='k', s=size)
+			ax.grid(b=True)
+			ax.set_xticklabels([])
+			ax.set_yticklabels([])
+			ax.set_title(r"$\varepsilon$=" + str(parameters[1]) + ", minsamples=" +
+				str(parameters[0]))
+	
+	
+	rc('font', **{'family':'serif','serif':['Palatino']})
+	rc('text', usetex=True)
+
+	plt.tight_layout(rect=[-0.05, 0, 1.05, 1])
+	plt.subplots_adjust(wspace=0)
+	
+	f.set_size_inches(5.9, 5.3)
+
+	plt.savefig(saveloc + "clusteringExamples.svg")
