53656da9022afb3f88537939c9c54dfa41e6648c
aajarven <anni.jarvenpaa@gmail.com>
2017-08-09 18:27:30 +0300

    ympyrät puolikkaiksi, tikit yms vielä työn alla

--- plotscripts/results/directionalHF.py
+++ plotscripts/results/directionalHF.py
@@ -17,6 +17,40 @@ from matplotlib import rc
 import numpy as np
 import sys
 
+import mpl_toolkits.axisartist.floating_axes as floating_axes
+from matplotlib.projections import PolarAxes
+
+#https://stackoverflow.com/a/44190325
+def setup_axes(fig, rect, maxR):
+    tr = PolarAxes.PolarTransform() 
+
+    theta0, theta1 = 0, np.pi
+    r0, r1 = 0, maxR
+    grid_helper = floating_axes.GridHelperCurveLinear(
+        tr, extremes=(theta0, theta1, r0, r1))
+
+    ax1 = floating_axes.FloatingSubplot(fig, rect, grid_helper=grid_helper)
+    fig.add_subplot(ax1)
+
+    # adjust axis
+    ax1.axis["left"].set_axis_direction("bottom")
+    ax1.axis["right"].set_axis_direction("top")
+    ax1.axis["bottom"].set_visible(False)
+    ax1.axis["top"].set_axis_direction("bottom")
+    ax1.axis["top"].toggle(ticklabels=True, label=True)
+    ax1.axis["top"].major_ticklabels.set_axis_direction("top")
+    ax1.axis["top"].label.set_axis_direction("top")
+#    ax1.axis["left"].label.set_text(r"z")
+#    ax1.axis["top"].label.set_text(r"RA")
+
+    # create a parasite axes whose transData in RA, cz
+    aux_ax = ax1.get_aux_axes(tr)
+
+    aux_ax.patch = ax1.patch  
+    ax1.patch.zorder = 0.9  
+
+    return ax1, aux_ax
+
 
 if __name__ == "__main__":
 #	inputfile = "../input/allButDuplicates-fullpath.txt" 
@@ -117,8 +151,8 @@ if __name__ == "__main__":
 			angleMask = np.array([direction < binlimits[angleIndex+1] and
 						 direction >= binlimits[angleIndex] for direction in
 						 directions])
-			print("[" + str(binlimits[angleIndex]) + ", " +
-		 str(binlimits[angleIndex+1]) + "]:\t" + str(np.sum(angleMask)) )
+#			print("[" + str(binlimits[angleIndex]) + ", " +
+#		 str(binlimits[angleIndex+1]) + "]:\t" + str(np.sum(angleMask)) )
 			
 			if np.sum(angleMask) < 10:
 				zeros[angleIndex].append(np.nan)
@@ -142,15 +176,28 @@ if __name__ == "__main__":
 	params = {'text.latex.preamble' : [r'\usepackage{wasysym}']}
 	plt.rcParams.update(params)
 
-	fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection='polar'))
+#	fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection='polar'))
 
-	print(h0s)
-	print(np.nanmean(h0s, axis=1))
-	ax1.plot(angles, np.nanmean(h0s, axis=1))
-	ax2.plot(angles, np.nanmean(zeros, axis=1))
+	fig = plt.figure()
+	ax1, aux_ax1 = setup_axes(fig, 211, 90)
+	ax1.axis["left"].label.set_text(r"$H_0$ (km/s/Mpc)")
+	ax1.axis["top"].label.set_text(r"$\phi$")
 
-#	plt.tight_layout()
-	fig.set_size_inches(5.9, 2.6)
+	aux_ax1.plot(angles, np.nanmean(h0s, axis=1))
+
+	ax2, aux_ax2 = setup_axes(fig, 212, 2.5)
+	aux_ax2.plot(angles, np.nanmean(zeros, axis=1))
+	ax2.axis["left"].label.set_text(r"$H_0$ (km/s/Mpc)")
+	ax2.axis["top"].label.set_text(r"$\phi$")
+
+#	print(h0s)
+#	print(np.nanmean(h0s, axis=1))
+#	ax1.plot(angles, np.nanmean(h0s, axis=1))
+#	ax2.plot(angles, np.nanmean(zeros, axis=1))
+#	ax2.set_xlim([0, np.pi])
+
+	plt.tight_layout()
+#	fig.set_size_inches(5.9, 2.6)
 
 #	plt.xlabel("Hubble flow zero point (Mpc from LG centre)")
 #	plt.ylabel("Combined mass of Milky Way and Andromeda (Solar masses)")
