f69677d0f86c2408a9948ddcba1750268c46a121
aajarven <anni.jarvenpaa@gmail.com>
2017-10-03 21:56:27 +0300

    toimii, roskaa joukossa

--- plotscripts/results/timingargument.py
+++ plotscripts/results/timingargument.py
@@ -4,35 +4,31 @@ from __future__ import print_function
 
 import logging
 from math import sin, cos, pi
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve#, minimize
 import sys
 
 def phiFunction(phi, radvel, distance, T):
-	return ((sin(phi)**2 - phi * sin(phi)) / ((1 + cos(phi))**2) -
-		 radvel*1.0/distance*T)
+#	return ((sin(phi)**2 - phi * sin(phi)) / ((1 + cos(phi))**2) -
+#		 radvel*1.0/distance*T)
+	return (distance*1.0/T*sin(phi)*(phi-sin(phi))/((1-cos(phi))**2)-radvel)
 
 def timingArgumentMass(radvel, distance, T):
 	G =	4.498768e-6 # kpc^3/Gyr^2/Msun
-
 	
-
-	phi2 = fsolve(phiFunction, 0, args=(radvel, distance, T))
-	if len(phi2) != 1:
+#	phi = minimize(phiFunction, 4.0, args=(radvel, distance, T)).x
+	phi = fsolve(phiFunction, 4.11, args=(radvel, distance, T))
+	if len(phi) != 1:
 		logging.exception("Unexpected length (" + str(len(phi)) + ") for " + 
-					"solution vector of phi2")
+					"solution vector of phi")
 		sys.exit(1)
-	phi2 = (phi2 % (2*pi))[0]
-	E2 = (phi2 + pi) % (2*pi)	# eccentric anomaly
-	print(E2)
+	phi = (phi % (2*pi))[0]
 
-	Omega = (E2 - sin(E2) - pi) / T
-	print(Omega)
+	a = distance / (1-cos(phi))
+#	print("a="+str(a))
 
-	a = distance / (1-cos(E2))
-	print(a)
-
-	M = Omega**2*a**3/G
-	print("{:.2e}".format(M))
+	M = a**3/G*(phi-sin(phi))**2/(T**2)
+#	print("{:.2e}".format(M))
+	return M
 
 if __name__ == "__main__":
-	timingArgumentMass(-125.0, 650.0, 13.6)
+	print("{:.2e}".format(timingArgumentMass(-119.0, 730.0, 20.0)))
