60d1062be77788d9a3434a41f98c305156255bd8
aajarven <anni.jarvenpaa@gmail.com>
2018-04-05 00:07:41 +0300

    TLS added, line slope reduced

--- plotscripts/background/linregressions.py
+++ plotscripts/background/linregressions.py
@@ -6,23 +6,36 @@ from matplotlib import rc
 import pylab
 import random
 from scipy.stats import linregress
-
+from scipy import odr
 
 def calculateY(x, slope, intercept):
 	return intercept + slope * x
+
+def line(B, x):
+	return B[0]*x + B[1]
+
+def orthogonalIntercept(x0, y0, k, b):
+	"""
+	Finds the point at which a line orthogonal to y = kx + b and passing
+	through point (x0, y0) intercepts the original line
+	"""
+	x = x0 + (y0*k - b*k - x0*k**2)/(k**2 + 1)
+	y = b + x0*k + (y0*k**2 - b*k**2 - x0*k**3)/(k**2 + 1)
+	return (x, y)
 
 if __name__ == "__main__":
 	np.random.seed(7155)
 	saveloc_OLS = "../../kuvat/OLS.svg"
 	saveloc_OLSproblem = "../../kuvat/OLSproblem.svg"
+	saveloc_TLS = "../../kuvat/TLS.svg"
 
 	N = 30
 	x = np.random.uniform(-10.0, 10.0, N)
-	y = [xx + np.random.normal(0.0, 3) for xx in x]
+	y = [xx - 0.2*xx + np.random.normal(0.0, 3) for xx in x]
 
 	slope1, intercept1, r_value1, p_value1, std_err1 = linregress(x,y)
 	slope2, intercept2, r_value2, p_value2, std_err2 = linregress(y,x)
-	plotxlimit = 11
+	plotxlimit = 12
 
 	# minimized distances
 
@@ -62,9 +75,7 @@ if __name__ == "__main__":
 		 color='k', linewidth=2.0, linestyle="--",
 		 label="fit using horizontal\ndistances")
 
-#	plt.legend(loc=2)
-	ax.legend(bbox_to_anchor=(1.23, 0.35), frameon=False)
-#	ax.legend(bbox_to_anchor=(0.78, 1.06), frameon=False)
+	ax.legend(bbox_to_anchor=(1.02, 0.35), frameon=False)
 
 	ax.set_xlim(-1*plotxlimit, plotxlimit)
 	ax.set_ylim(min(y)-0.5, max(y)+0.5)
@@ -77,8 +88,34 @@ if __name__ == "__main__":
 
 	plt.margins(y=0.3)
 	plt.tight_layout()
-#	plt.subplots_adjust(wspace=0)
 	plt.axis('off')
 	f.set_size_inches(3.5, 3)
 	plt.savefig(saveloc_OLSproblem)
 
+	plt.cla()
+	plt.clf()
+
+	# TLS
+	linear_model = odr.Model(line)
+	data = odr.Data(x, y)
+	linear_odr = odr.ODR(data, linear_model, beta0=[slope1, intercept1])
+	slope3, intercept3 = linear_odr.run().beta
+	
+	f = plt.figure()
+	ax = f.add_subplot(111)
+	ax.scatter(x, y, color='k', s=20, zorder=3)
+	ax.plot([-plotxlimit, plotxlimit], [intercept3 - slope3 * plotxlimit,
+									 intercept3 + slope3 * plotxlimit],
+		 color='k', linewidth=2.0, zorder=2)
+	for x0, y0 in zip(x, y):
+		xx, yy = orthogonalIntercept(x0, y0, slope3, intercept3)
+		ax.plot([x0, xx], [y0, yy], color='k', zorder=1)
+
+	ax.set_xlim(-1*plotxlimit, plotxlimit)
+	ax.set_ylim(min(y)-0.5, max(y)+0.5)
+
+	plt.tight_layout()
+	plt.subplots_adjust(wspace=0)
+	plt.axis('off')
+	f.set_size_inches(3, 3)
+	plt.savefig(saveloc_TLS)
