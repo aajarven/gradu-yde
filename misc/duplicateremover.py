6497c1ecef168bb540afd40ecde64d0198ea46d8
aajarven <anni.jarvenpaa@gmail.com>
2018-04-06 00:05:14 +0300

    functionin duplicate remover

--- misc/duplicateremover.py
+++ misc/duplicateremover.py
@@ -15,4 +15,25 @@ if __name__ == "__main__":
 		print("Give input and output files as arguments")
 		sys.exit()
 
-	f = 
+	infile = args[0]
+	outfile = args[1]
+
+	try:
+		f = open(infile, 'r')
+	except IOError:
+		print("File {} cannot be opened".format(infile))
+	
+	lines = []
+	line = f.readline()
+	while len(line) > 0:
+		if line not in lines:
+			lines.append(line)
+		line = f.readline()
+	
+	f.close()
+	
+	f = open(outfile, 'w')
+	for line in lines:
+		f.write(line)
+	f.close()
+
