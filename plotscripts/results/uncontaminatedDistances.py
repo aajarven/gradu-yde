dcdc52a5afd71cb031bdecc06b427d929f2fe31c
aajarven <anni.jarvenpaa@gmail.com>
2018-05-02 22:06:05 +0300

    added unit to x-axis

--- plotscripts/results/uncontaminatedDistances.py
+++ plotscripts/results/uncontaminatedDistances.py
@@ -25,7 +25,7 @@ matplotlib.rcParams.update({'font.size': 13})
 f, ax = plt.subplots(1, 1)
 
 ax.plot(distances[::-1], indexes, color='k')
-ax.set_xlabel("Range containing only type 1 particles")
+ax.set_xlabel("Range containing only type 1 particles (Mpc)")
 ax.set_ylabel("Number of simulations")
 ax.set_xlim(min(distances), max(distances))
 ax.set_ylim(0, math.ceil(max(indexes)/10.0)*10)
