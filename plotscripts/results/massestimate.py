532a83c52e259a1d1c62cfe7a6fc9179ecd769c1
aajarven <anni.jarvenpaa@gmail.com>
2018-10-05 01:17:44 +0300

    training-testing split

--- plotscripts/results/massestimate.py
+++ plotscripts/results/massestimate.py
@@ -19,7 +19,7 @@ import numpy as np
 import os.path
 
 from sklearn import preprocessing
-#from sklearn import cross_validation
+from sklearn import cross_validation
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_decomposition import PLSRegression, PLSSVD
@@ -345,52 +345,51 @@ if __name__ == "__main__":
 	plt.gcf().set_size_inches(4.8, 3.5)
 	plt.tight_layout()
 	plt.savefig(outputdir + "rmse-alldata.pdf")
+	plt.cla()
+	plt.clf()
 
-#	n_folds = 10
-#	splitting_seed = 7
-#	
-#	fullData = np.concatenate((y.reshape(-1, 1), data), axis=1)
-#	X_train, X_test, Y_train, Y_test, timing_train, timing_test = train_test_split(data, y,
-#																				timingArgumentMasses,
-#																				test_size=0.25,
-#																				random_state=splitting_seed)
-#
-#
-#	scaler = preprocessing.StandardScaler().fit(X_train)
-#	X_train_scaled = scaler.transform(X_train)
-#	X_test_scaled = scaler.transform(X_test)
-#
-#	pca2 = PCA()
-#	X_train_reduced = pca2.fit_transform(X_train_scaled)
-#	X_test_reduced = pca2.transform(X_train_scaled)
-#
-#	# MSE in training using k-fold cross validation
-#	RMSEs = []
-#	regr = LinearRegression()
-#	rkf = RepeatedKFold(n_splits=n_folds, n_repeats=n_repeats, random_state=kfold_seed)
-#	for i in range(10):
-#		score = cross_val_score(regr, X_train_reduced[:,:i+1],
-#							  Y_train.ravel(),
-#							  cv=rkf,
-#							  scoring='neg_mean_squared_error').mean()
-#		RMSEs.append(sqrt(-score))
-#	
+	
+	### training and testing set ###
+	pca2 = PCA()
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(data,
+																	  y,
+																	  test_size=0.4,
+																	  random_state=kfold_seed)
+	X_train_reduced = pca2.fit_transform(scale(X_train))
+	n = len(X_train_reduced)
+	kfold2 = cross_validation.KFold(n, n_folds=n_folds, shuffle=True,
+								 random_state=kfold_seed)
+
+	# RMSE in training using k-fold cross validation
+	RMSEs_train = []
+	regr = LinearRegression()
+	
+	for i in range(variables):
+		score = model_selection.cross_val_score(regr, X_train_reduced[:,:i+1],
+							  y_train,
+							  cv=kfold2,
+							  scoring='neg_mean_squared_error').mean()
+		RMSEs_train.append(sqrt(-score))
+
+			
+
+	
 #	# TA comparison
 #	timing_mse = mean_squared_error(Y_train, timing_train)
 #
 #
-#	plt.plot(np.arange(1, 11), RMSEs, '-o', color='k')
+	plt.plot(range(1, variables+1), RMSEs_train, '-o', color='k')
 #	plt.plot([1, 10], [sqrt(timing_mse), sqrt(timing_mse)], color='r')
-#
-#	#plt.ylim(0, 1.3)
-#	plt.xlabel("Number of PCs in regression")
-#	lims = plt.xlim()
-#	plt.xticks(np.arange(ceil(lims[0]), ceil(lims[1]), 1))
-#	plt.ylabel(r"RMSE ($M_{\astrosun}$)")
-#	plt.title("Training error using " + str(n_folds) + " folds and " +
-#		   str(n_repeats) + " repeats")
-#	plt.gca().set_ylim(bottom=0)
-##	plt.gca().set_ylim(top=1.3)
-#	plt.savefig(outputdir + "training-RMSE.pdf")
-#	plt.cla()
-#	plt.clf()
+
+	#plt.ylim(0, 1.3)
+	plt.xlabel("Number of PCs in regression")
+	lims = plt.xlim()
+	plt.xticks(np.arange(ceil(lims[0]), ceil(lims[1]), 1))
+	plt.ylabel(r"RMSE ($M_{\astrosun}$)")
+	plt.title("Training error using " + str(n_folds) + " folds")
+	plt.gca().set_ylim(bottom=0)
+#	plt.gca().set_ylim(top=1.3)
+	plt.savefig(outputdir + "training-RMSE.pdf")
+	plt.cla()
+	plt.clf()
+	
